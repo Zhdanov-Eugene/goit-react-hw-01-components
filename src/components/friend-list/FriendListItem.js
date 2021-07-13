456920609f0c81e9c724fe344a8145eb4991e460
EugeneZhdanov <jeka7753191@gmail.com>
2021-07-13 17:36:37 +0300

    added homework prerequisites

---
+++ src/components/friend-list/FriendListItem.js
@@ -0,0 +1,8 @@
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="" width="48" />
+  <p class="name"></p>
+</li>
+// import friends from 'путь/к/friends.json';
+
+// <FriendList friends={friends} />,
